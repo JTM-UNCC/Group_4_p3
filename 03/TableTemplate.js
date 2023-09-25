90da67f762cace2385b2f744b81cc22555d42b4d
Shreeya K <skarnam1@uncc.edu>
2023-09-25 14:20:48 -0400

    code

--- 03/TableTemplate.js
+++ 03/TableTemplate.js
@@ -1,60 +1,44 @@
+'use strict';
+
 class TableTemplate {
-    static fillIn(id, dict, columnName) {
-        const tableElem = document.getElementById(id);
-        if (!tableElem) {
-            console.error(`Table with id '${id}' not found.`);
+    static fillIn(tableId, dictionary, columnName = null) {
+        const table = document.getElementById(tableId);
+
+        if (!table) {
+            console.error(`Table with id "${tableId}" not found.`);
             return;
         }
 
-        const headerElem = tableElem.rows[0];
-
-        let match = false;
-        let colNum;
-        const numCols = headerElem.cells.length;
-
-        for (let j = 0; j < numCols; j++) {
-            const currColName = headerElem.cells[j].innerHTML;
-            const filledCurrColName = TableTemplate.processTemplate(currColName, dict);
-
-            if (filledCurrColName === columnName) {
-                match = true;
-                colNum = j;
-            }
-
-            headerElem.cells[j].innerHTML = filledCurrColName;
+        const headerRow = table.rows[0];
+        for (let i = 0; i < headerRow.cells.length; i++) {
+            const cell = headerRow.cells[i];
+            cell.textContent = this.processTemplate(cell.textContent, dictionary);
         }
 
-        const numRows = tableElem.rows.length;
-
-        if (!columnName) {
-            for (let i = 1; i < numRows; i++) {
-                for (let j = 0; j < numCols; j++) {
-                    const currCell = tableElem.rows[i].cells[j];
-                    currCell.innerHTML = TableTemplate.processTemplate(currCell.innerHTML, dict);
+        let columnIndex = -1;
+        if (columnName) {
+            for (let i = 0; i < headerRow.cells.length; i++) {
+                if (headerRow.cells[i].textContent === columnName) {
+                    columnIndex = i;
+                    break;
                 }
             }
-            return;
         }
 
-        if (!match) {
-            return;
-        }
-
-        if (columnName) {
-            for (let i = 1; i < numRows; i++) {
-                const currCell = tableElem.rows[i].cells[colNum];
-                currCell.innerHTML = TableTemplate.processTemplate(currCell.innerHTML, dict);
+        if (columnIndex !== -1) {
+            for (let i = 1; i < table.rows.length; i++) {
+                const cell = table.rows[i].cells[columnIndex];
+                cell.textContent = this.processTemplate(cell.textContent, dictionary);
             }
         }
+
+        table.style.visibility = 'visible';
     }
 
-    static processTemplate(template, dict) {
-        return template.replace(/{{(.*?)}}/g, (match, property) => {
-            if (dict.hasOwnProperty(property)) {
-                return dict[property];
-            } else {
-                return '';
-            }
+    static processTemplate(text, dictionary) {
+        return text.replace(/{{(.*?)}}/g, (match, property) => {
+            const value = dictionary[property];
+            return value !== undefined ? value : '';
         });
     }
 }
