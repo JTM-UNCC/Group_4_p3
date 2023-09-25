ffb4e4788ceab789f94d84d9abe925b73364b8ca
calvell0 <143210595+calvell0@users.noreply.github.com>
2023-09-25 16:04:05 -0400

    fixed linter errors in DatePicker.js

--- 03/DatePicker.js
+++ 03/DatePicker.js
@@ -2,11 +2,7 @@
 
 class DatePicker{
 
-    id;
-    element;
-    date;
-    callback;
-    selectedDate;
+
     constructor(id, callback) {
         this.id = id;
         this.element = document.getElementById(id);
@@ -14,11 +10,9 @@ class DatePicker{
     }
 
     render(date, selectedElement){
-        if (!date instanceof Date){
-            return;
-        }
 
-        if (this.date != null && this.date.getMonth() === date.getMonth() && this.date.getFullYear() === date.getFullYear()){
+        if (this.date != null && this.date.getMonth() === date.getMonth()
+            && this.date.getFullYear() === date.getFullYear()){
             this.selectedDate.classList.remove('selected');
             this.selectedDate = selectedElement;
             selectedElement.setAttribute('class', 'selected');
@@ -31,20 +25,14 @@ class DatePicker{
         this.element.appendChild(table);
     }
 
-    /**
-     * gets the number of days in a given month
-     * @param date date object in the month that we want the number of days from
-     */
-    daysInMonth(date){
-        return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-    }
+
     createTable(){
 
         const table = document.createElement('table');
-        table.appendChild(this.createCaption(this.date));
+        table.appendChild(this.createCaption());
         const header = this.createHeader();
         table.appendChild(header);
-        const body = this.createBody()
+        const body = this.createBody();
         table.appendChild(body);
         return table;
 
@@ -56,7 +44,7 @@ class DatePicker{
      * gets first row of header, nav controls
      * @returns array of values of each cell in the row
      */
-    header1(){
+    static header1(){
         return [{value: "<-", active: true},
             {value: "\u00a0", active: false},
             {value: "\u00a0", active: false},
@@ -70,19 +58,19 @@ class DatePicker{
      * gets second row of header, the days of the week
      * @returns array of values of the cells in the row
      */
-    daysOfWeek(){
+    static daysOfWeek(){
         return [{value: 'Sun', active: false},
             {value: 'Mon', active: false},
             {value: 'Tue', active: false},
             {value: 'Wed', active: false},
             {value: 'Thu', active: false},
             {value: 'Fri', active: false},
-            {value: 'Sat', active: false},]
+            {value: 'Sat', active: false}];
     }
-    createCaption(date){
-        const month = date.toLocaleString('default', { month: 'long' });
+    createCaption(){
+        const month = this.date.toLocaleString('default', { month: 'long' });
         const caption = document.createElement('caption');
-        const text = document.createTextNode(month + " " + date.getFullYear());
+        const text = document.createTextNode(month + " " + this.date.getFullYear());
         caption.appendChild(text);
         return caption;
     }
@@ -90,7 +78,8 @@ class DatePicker{
     createBody(){
         const firstDayOfMonth = new Date(this.date.getFullYear(), this.date.getMonth(), 1);
         const month = firstDayOfMonth.getMonth();
-        const tempDate = new Date(firstDayOfMonth.getFullYear(), firstDayOfMonth.getMonth(), 1 - firstDayOfMonth.getDay());
+        const tempDate = new Date(firstDayOfMonth.getFullYear(),
+            firstDayOfMonth.getMonth(), 1 - firstDayOfMonth.getDay());
         const body = document.createElement("tbody");
 
         for(let i = 0; i < 7; i++){
@@ -100,7 +89,7 @@ class DatePicker{
 
             const rowContent = [];
             for (let k = 0; k < 7; k++){
-                rowContent.push({ value: tempDate.getDate(), active: tempDate.getMonth() === month });
+                rowContent.push({value: tempDate.getDate(), active: tempDate.getMonth() === month});
                 tempDate.setDate(tempDate.getDate() + 1);
             }
             const row = this.getRow(rowContent, 'td');
@@ -116,29 +105,32 @@ class DatePicker{
     getRow(values, cellType){
         const row = document.createElement('tr');
         for (const val in values){
-            const cell = document.createElement(cellType);
-            cell.appendChild(document.createTextNode(values[val].value));
-            row.appendChild(cell);
-            if(values[val].active) cell.setAttribute('class', 'active');
-            if (values[val].active && cellType !== 'th'){
+            if (Object.hasOwn(values, val)) {
+                const cell = document.createElement(cellType);
+                cell.appendChild(document.createTextNode(values[val].value));
+                row.appendChild(cell);
+                if (values[val].active) cell.setAttribute('class', 'active');
+                if (values[val].active && cellType !== 'th') {
 
-                if(values[val].value === this.date.getDate()){
-                    cell.setAttribute('class', 'selected');
-                    this.selectedDate = cell;
+                    if (values[val].value === this.date.getDate()) {
+                        cell.setAttribute('class', 'selected');
+                        this.selectedDate = cell;
+                    }
+                    cell.addEventListener('click', (evt) => {
+                        const tempDate = new Date(this.date.getFullYear(),
+                            this.date.getMonth(), parseInt(evt.target.textContent, 10));
+                        this.render(tempDate, evt.target);
+                        this.callback(this.id, ({
+                                month: tempDate.getMonth() + 1,//month value is 0-indexed by default
+                                day: tempDate.getDate(),
+                                year: tempDate.getFullYear()
+                            })
+                        );
+                    });
+                } else if (values[val].active === false && cellType !== 'th') {
+                    cell.setAttribute('class', 'inactive');
                 }
-                cell.addEventListener('click', (evt) => {
-                    const tempDate = new Date(this.date.getFullYear(), this.date.getMonth(), parseInt(evt.target.textContent));
-                    this.render(tempDate, evt.target);
-                    this.callback(this.id, ({ month: tempDate.getMonth() + 1, //month value is 0-indexed by default
-                        day: tempDate.getDate(),
-                        year: tempDate.getFullYear()})
-                    );
-                });
             }
-            else if (values[val].active === false && cellType !== 'th'){
-                cell.setAttribute('class', 'inactive');
-            }
-
         }
         return row;
     }
@@ -148,15 +140,15 @@ class DatePicker{
     createHeader(){
         const head = document.createElement("thead");
 
-        const row1 = this.getRow(this.header1(), 'th');
+        const row1 = this.getRow(DatePicker.header1(), 'th');
         row1.children[0].addEventListener('click', () => {
-            this.render(new Date(this.date.getFullYear(), this.date.getMonth() - 1, 1))
+            this.render(new Date(this.date.getFullYear(), this.date.getMonth() - 1, 1));
         });
         row1.children[6].addEventListener('click', () => {
-            this.render(new Date(this.date.getFullYear(), this.date.getMonth() + 1, 1))
+            this.render(new Date(this.date.getFullYear(), this.date.getMonth() + 1, 1));
         });
         head.appendChild(row1);
-        head.appendChild(this.getRow(this.daysOfWeek(), 'th'));
+        head.appendChild(this.getRow(DatePicker.daysOfWeek(), 'th'));
         return head;
     }
 
